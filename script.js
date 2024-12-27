848760f80712cb13fed5a9d1c7f872fe1a16dcef
Tlalla <taqievalala@gmail.com>
2024-12-27 13:39:59 +0400

    son

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ let changing=document.querySelector('.changing');
 let thirdartwords=document.querySelector('.thirdartwords');
 let allcoords=document.querySelector('.allcoords');
 let withphoto=document.querySelector('.withphoto');
+const sendButton = document.querySelector('.send');
+const spikername = document.querySelector('.spikername');
+const spikersurname = document.querySelector('.spikersurname');
+const spikerfaculty = document.querySelector('.spikerfaculty');
 const imageElement = document.getElementById("changing-image");
+const spikergroup = document.querySelector('.spikergroup');
+const spikeryear = document.querySelector('.spikerstudyyear');
+const spikernumber= document.querySelector('.spikernumber');
+let fourthart=document.querySelector('.fourthart');
+let resubmit=document.querySelector('.resubmit');
+let successful= document.querySelector('.successful');
+let backbutton=document.querySelector('.backbutton');
+let resend=document.querySelector('.resend');
+let cooperation=document.querySelector('.cooperation');
+let fifthart=document.querySelector('.fifthart');
+let suggestions=document.querySelector('.suggestions');
+let yeardiv=document.querySelector('.yeardiv');
+let form=document.querySelector('form');
+let menu=document.querySelector('.menu');
+let others=document.querySelector('.others')
 tour.addEventListener('mouseover',()=>{
     tours.classList.remove('none');
 })
@@ -77,61 +96,132 @@ function changeLanguage(selectElement) {
       window.location.href = './index.html';
     }
   }
-// fetch('https://sheetdb.io/api/v1/kp4l6j7im8058')
-//   .then(response => {
-//     if (!response) {
-//       throw new Error('Şəbəkə xətası baş verdi');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log(data); 
-//         thirdartwords.prepend(names);
-//         thirdartwords.append(years);
-//         thirdartwords.append(roles);
-//         withphoto.insertBefore(coords,thirdartwords);
-//         i=0;
-//         names.textContent = `${data[i].name} ${data[i].surname}`;
-//         years.textContent = ` Years in club : ${data[i].years} years`; 
-//         roles.textContent = ` Role : ${data[i].role}`; 
-//         coords.src = data[i].photo;
-//         coords.alt="photo";
-//         right.addEventListener('click',()=>{
-//             if (i < data.length-1) {
-//                 i++
-//                 names.textContent = `${data[i].name} ${data[i].surname}`;
-//                 years.textContent = ` Years in club : ${data[i].years} years`; 
-//                 roles.textContent = ` Role : ${data[i].role}`; 
-//         coords.src = data[i].photo;
+// fetch('https://sheetdb.io/api/v1/7axaezay79gf4')
+  .then(response => {
+    if (!response) {
+      throw new Error('Şəbəkə xətası baş verdi');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); 
+        thirdartwords.prepend(names);
+        thirdartwords.append(years);
+        thirdartwords.append(roles);
+        withphoto.insertBefore(coords,thirdartwords);
+        i=0;
+        names.textContent = `${data[i].name} ${data[i].surname}`;
+        years.textContent = ` Years in club : ${data[i].years} years`; 
+        roles.textContent = ` Role : ${data[i].role}`; 
+        coords.src = data[i].photo;
+        coords.alt="photo";
+        right.addEventListener('click',()=>{
+            if (i < data.length-1) {
+                i++
+                names.textContent = `${data[i].name} ${data[i].surname}`;
+                years.textContent = ` Years in club : ${data[i].years} years`; 
+                roles.textContent = ` Role : ${data[i].role}`; 
+        coords.src = data[i].photo;
 
-//               }else{
-//               i=0; 
-//              names.textContent=`${data[i].name} ${data[i].surname}`
-//              years.textContent = ` Years in club : ${data[i].years} years`; 
-//              roles.textContent = ` Role : ${data[i].role}`; 
-//              coords.src = data[i].photo;
+              }else{
+              i=0; 
+             names.textContent=`${data[i].name} ${data[i].surname}`
+             years.textContent = ` Years in club : ${data[i].years} years`; 
+             roles.textContent = ` Role : ${data[i].role}`; 
+             coords.src = data[i].photo;
 
-//               }});
-//         left.addEventListener('click',()=>{
-//             if (i>0) {
-//                 i--;
-//                 names.textContent = `${data[i].name} ${data[i].surname}`;
-//                 years.textContent = ` Years in club : ${data[i].years} years`; 
-//                 roles.textContent = ` Role : ${data[i].role}`;  
-//                 coords.src = data[i].photo;
+              }});
+        left.addEventListener('click',()=>{
+            if (i>0) {
+                i--;
+                names.textContent = `${data[i].name} ${data[i].surname}`;
+                years.textContent = ` Years in club : ${data[i].years} years`; 
+                roles.textContent = ` Role : ${data[i].role}`;  
+                coords.src = data[i].photo;
 
                 
-//               }else{
-//               i=7; 
-//              names.textContent=`${data[i].name} ${data[i].surname}`;
-//              years.textContent = ` Years in club : ${data[i].years} years`; 
-//              roles.textContent = ` Role : ${data[i].role}`; 
-//              coords.src = data[i].photo;
+              }else{
+              i=7; 
+             names.textContent=`${data[i].name} ${data[i].surname}`;
+             years.textContent = ` Years in club : ${data[i].years} years`; 
+             roles.textContent = ` Role : ${data[i].role}`; 
+             coords.src = data[i].photo;
 
-//               }});
-//         })
+              }});
+        })
+let registrationData=[];
+sendButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    // console.log(!spikername.value.trim)
+    if (!!spikername.value.trim() && !!spikersurname.value.trim() && !!spikerfaculty.value.trim() && !!spikergroup.value.trim() && !!spikeryear.value.trim() && !!spikernumber.value.trim()) {
+    registrationData.push({
+        name: spikername.value,
+        surname: spikersurname.value,
+        faculty: spikerfaculty.value,
+        groupnumber: spikergroup.value,
+        year: spikeryear.value,
+        phone: spikernumber.value
+    });
+    console.log(registrationData);
+    successful.classList.remove('none');
+    fourthart.classList.add('none');
+}else{
+    alert("Please fill out all fields.");
+}
 
-        
-    // const changeName =()=>{
-        
-    // };
+})
+resubmit.addEventListener('click', ()=>{
+    
+    successful.classList.add('none');
+    fourthart.classList.remove('none');
+    spikername.value='',
+        spikersurname.value='',
+        spikerfaculty.value='',
+        spikergroup.value='',
+        spikeryear.value='',
+        spikernumber.value=''
+})
+let suggestionData=[];
+backbutton.addEventListener('click', function(event) {
+    event.preventDefault();
+    if(!!suggestions.value.trim()){
+        suggestionData.push({
+            feedback: suggestions.value,
+        });
+        console.log(suggestionData);
+        cooperation.classList.remove('none');
+        fifthart.classList.add('none');
+    }
+    
+})
+resend.addEventListener('click', ()=>{
+    cooperation.classList.add('none');
+    fifthart.classList.remove('none');
+    suggestions.value='';
+})
+
+let yearerror=document.createElement('p');
+yearerror.classList.add('rederror');
+spikeryear.addEventListener('input', function () {
+    const value = parseInt(this.value, 10);
+    if (value < 1 || value > 4) {
+        yeardiv.append(yearerror);
+        yearerror.textContent="Year of study must be between 1 and 4."
+        this.value = '';
+    }
+});
+spikernumber.addEventListener('input', () => {
+    spikernumber.value = spikernumber.value.replace(/[^0-9+]/g, '');
+});
+spikerfaculty.addEventListener('input', () => {
+    spikerfaculty.value = spikerfaculty.value.replace(/[0-9]/g, '');
+});
+spikername.addEventListener('input', () => {
+    spikername.value = spikername.value.replace(/[0-9]/g, '');
+});
+spikersurname.addEventListener('input', () => {
+    spikersurname.value = spikersurname.value.replace(/[0-9]/g, '');
+});
+menu.addEventListener('click',()=>{
+    others.classList.toggle('none');
+})
